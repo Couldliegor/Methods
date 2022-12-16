bb969dfaf84771255802e440b28f0b33d4bbaf17
egorcouldlie <couldliegor@gmail.com>
2022-12-16 14:33:55 +0300

    Task 2

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -12,12 +12,56 @@ public class Main {
         return some;
     }
 
+    public static int OC(int OS, int year) {
+        boolean goodVersion = year >= 2015;
+        boolean earlyVersion = year < 2015;
+        if (goodVersion) {
+            switch (OS) {
+                case 0:
+                    System.out.println("Download version for iOS from this link: ");
+                    break;
+                case 1:
+                    System.out.println("Download version for Android from this link: ");
+                    break;
+                default:
+                    System.out.println("Sorry, we haven't done a website for your OS yet!");
+            }
+        } else if (earlyVersion) {
+            switch (OS) {
+                case 0:
+                    System.out.println("Download light version from this link: ");
+                    break;
+                case 1:
+                    System.out.println("Download light version for Android from this link: ");
+                    break;
+                default:
+                    System.out.println("Sorry, we haven't done a website for your OS yet!");
+                    //push
+            }
+        }
+        return OS;
+    }
     public static void main(String[] args) {
         task1();
+        task2();
     }
 
     public static void task1() {
-        int i = 2100; // enter year here
+        System.out.println("TASK1");
+        int i = (int) (Math.random() * 4000); // enter year here
         year(i);
     }
+
+    public static void task2() {
+        System.out.println("\nTASK2");
+        int version = 1;//enter version here
+        int minYear = 1975;
+        int maxYear = 2125;
+        int middleYear = maxYear - minYear;
+        int year = (int) (Math.random() * ++middleYear) + minYear;
+        System.out.println(year);
+        //enter 0 or 1 here
+        OC(version, year);
+    }
+//    hardly done, actually donno how to change an int version in method OC, to massive int[] smth = new int[1]; smth[0] = 0;//or 1 if you want ))
 }
