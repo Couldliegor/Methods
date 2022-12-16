a934bc2cc63a1088b93f9dce3670395c32817014
egorcouldlie <couldliegor@gmail.com>
2022-12-16 12:56:00 +0300

    Task 1

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -1,7 +1,23 @@
 package Main;
 
 public class Main {
+    public static int year(int some) {
+        if (some % 100 == 0 && some % 400 != 0) {
+            System.out.println(some + " year is not leap");
+        } else if (some % 4 == 0) {
+            System.out.println(some + " year is leap!");
+        } else {
+            System.out.println(some + " year is not leap");
+        }
+        return some;
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task1();
+    }
+
+    public static void task1() {
+        int i = 2100; // enter year here
+        year(i);
     }
 }
